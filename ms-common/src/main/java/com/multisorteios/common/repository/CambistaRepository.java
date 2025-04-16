37e484911d9288a3b2fe150673037ea31e592394
edeniltonfb <edeniltonfb@gmail.com>
2025-04-16 14:14:45 -0300

    ajueste

--- ms-common/src/main/java/com/multisorteios/common/repository/CambistaRepository.java
+++ ms-common/src/main/java/com/multisorteios/common/repository/CambistaRepository.java
@@ -2,6 +2,7 @@ package com.multisorteios.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.multisorteios.common.model.Cambista;
@@ -10,6 +11,6 @@ import com.multisorteios.common.model.Cambista;
 public interface CambistaRepository extends JpaRepository<Cambista, Integer>{
 
 	@Query(value = "SELECT * FROM CAMBISTA WHERE EMPRESA_ID = :empresaId AND LOGIN = :login", nativeQuery = true)
-	Cambista findByEmpresaLogin(Integer empresaId, String login);
+	Cambista findByEmpresaLogin(@Param("empresaId") Integer empresaId, @Param("login")String login);
 
 }
